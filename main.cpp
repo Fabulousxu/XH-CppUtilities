181c0072c304e0f2a5818f265b8e76a875888b26
H.Z.W. <150144387+Hzwx158@users.noreply.github.com>
2024-08-09 21:09:30 +0800

    初步完善predicate
    predicate部分初步完善，仍有需要完善的地方：
    1. T的临时变量不能被传给下面一个接受T&为第一个参数的predicate
    2. 对于const T &等的处理

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "promise.h"
 #include "enum_name.h"
-#include "member_function.h"
+#include "function.h"
 
 struct A {
   int add(int a, int b) { return a + b; }
@@ -10,7 +10,8 @@ struct A {
   int div(int a, int b) const volatile { return a / b; }
 };
 
-int main() {
+//===========================mem_fun===========================
+int mem_fun_example() {
   xh::mem_fun add = &A::add;
   xh::mem_fun sub = &A::sub;
   xh::mem_fun mul = &A::mul;
@@ -25,5 +26,45 @@ int main() {
   mul(c, 2, 1);
   div(d, 2, 1);
 
+  return 0;
+}
+
+
+//===========================pred===========================
+#include <vector>
+#include "predicate.h"
+std::vector<int> only_odd(std::vector<int> &vec){
+  std::vector<int> res{};
+  for(auto &num:vec){
+    if(num&1)
+      res.push_back(num);
+  }
+  return res;
+}
+void print_vec(const std::vector<int> &arr){
+  printf("length:%d{\n", arr.size());
+  for(auto &n:arr){
+    printf("%d,\n", n);
+  }
+  printf("}\n");
+}
+int test_pred(){
+  std::vector<int> arr{1,2,3,4,5,6,7,8,9,10}; //array for test
+  const char str[] = "call add_b"; // a local variance for test
+  // one way to define predicate
+  auto add_b = xh::predicate([&str](std::vector<int> vec, int b)->std::vector<int>{
+    for(auto &num:vec)
+      num+=b;
+    printf("%s\n", str);
+    return vec;
+  });
+  //another way to define predicate
+  auto onlyOdd = PRED(only_odd);
+  //something like C++20
+  arr | onlyOdd | add_b(10) | PRED(print_vec);
+  return 0;
+}
+int main(){
+  test_pred();
   return 0;
 }
--- predicate.h
+++ predicate.h
@@ -1,20 +1,30 @@
 #ifndef HZWXLIB_HSTD_HPREDICATE_H
 #define HZWXLIB_HSTD_HPREDICATE_H
 #include <tuple>
-namespace hstd{
+#include "./function.h"
+#include <functional>
+namespace xh{
+template<class F>
+inline auto stdFunc(F func){return std::function<function_traits_t<F>>(func);}
 
 /**
  * @brief 谓词Caller(C++20管道运算符)
- * @tparam TFunc 函数类型
- * @tparam Args 函数剩余参数
+ * @tparam Func 原函数的类型
+ * @tparam Args 函数除第一个参数以外的剩余参数类型
 */
-template<class TFunc, class ...Args>
-class TPredicateCaller{
+template<class Func, class ...Args>
+class predicate_no_call{
 private:
-    TFunc __func;
+    Func __func;
     std::tuple<Args...> __args;
 public:
-    TPredicateCaller(TFunc func, std::tuple<Args...> &&args):__func(func), __args(std::move(args)){}
+    using subject_type = typename function_traits<Func>::argument_type<0>;
+    predicate_no_call(Func func, std::tuple<Args...> &&args)
+        :__func(func), __args(std::move(args)){}
+    predicate_no_call(const predicate_no_call &)=delete;
+    predicate_no_call(predicate_no_call &&)=default;
+    predicate_no_call &operator=(const predicate_no_call &)=delete;
+    predicate_no_call &operator=(predicate_no_call &&)=default;
     /**
      * @brief 谓词调用(有参版)
      * @param subject 主语（函数第一个函数）
@@ -23,38 +33,46 @@ public:
      * @attention 主语是C数组、参数是指针时，要么数组写成arr+0的形式，要么参数指针写成const
      * @attention 不得更改“主语”，相当于是“主语”的const成员函数
     */
-    template<class T>
-    friend auto operator|(const T &subject, const TPredicateCaller<TFunc, Args...> &pred){
+    friend function_return_t<Func> operator|(
+        subject_type subject, 
+        const predicate_no_call<Func, Args...> &pred
+    ){
         auto args=std::tuple_cat(std::make_tuple(subject), pred.__args);
         return std::apply(pred.__func, args);
     }
 };
-
+template<class Func, class ...Args>
+predicate_no_call(Func, std::tuple<Args...>) -> predicate_no_call<std::function<function_traits_t<Func>>, Args...>;
 /**
  * @brief 谓词Wrapper(C++20管道运算符)
  * @tparam TFunc 函数类型
  */
-template<class TFunc>
-class TPredicateWrapper
+template<class Func>
+class predicate
 {
 private:
-    TFunc __func;
+    Func __func;
 public:
+    using subject_type = typename function_traits<Func>::argument_type<0>;
     /**
      * @brief 构造函数，把一个函数打包成“谓词”形式
      * @param func 被打包函数指针
      * @attention 不得更改“主语”，相当于是“主语”的const成员函数
     */
-    TPredicateWrapper(TFunc func):__func(func){}
+    predicate(Func func):__func(func){}
+
+    predicate(const predicate &)=delete;
+    predicate(predicate &&)=default;
+    predicate &operator=(const predicate &)=delete;
+    predicate &operator=(predicate &&)=default;
     /**
      * @brief 谓词补充
      * @param args 除了第一个参数以外的其余参数
      * @return 返回Caller
     */
     template<class ...Args>
-    TPredicateCaller<TFunc, Args...> operator()(Args... args) const {
-        TPredicateCaller<TFunc, Args...> res(__func,std::make_tuple(args...));
-        return res;
+    predicate_no_call<Func,Args...> operator()(Args... args) const {
+        return predicate_no_call<Func, Args...>(__func,std::make_tuple(args...));
     }
     /**
      * @brief 谓词调用(无参版)
@@ -64,11 +82,17 @@ public:
      * @attention 主语是C数组、参数是指针时，要么数组写成arr+0的形式，要么参数指针写成const
      * @attention 不得更改“主语”，相当于是“主语”的const成员函数
     */
-    template<class T>
-    friend auto operator|(const T &subject, const TPredicateWrapper<TFunc> &pred){
+    friend function_return_t<Func> operator|(
+        subject_type subject, 
+        const predicate<Func> &pred
+    ){
         return pred.__func(subject);
     }
+    
 };
-#define PRED hstd::TPredicateWrapper
+template<class Func>
+predicate(Func) -> predicate<std::function<function_traits_t<Func>>>;
+
+#define PRED xh::predicate
 }
 #endif
