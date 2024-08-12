7c55847ee87a53281d5d2856825e02b7fd9a8cb8
Fabulousxu <1292248830@qq.com>
2024-08-12 19:40:11 +0800

    完成了callable_traits

--- function.h
+++ function.h
@@ -1,111 +1,139 @@
-#ifndef FUNCTION_TRAITS_H
-#define FUNCTION_TRAITS_H
+#ifndef _XH_FUNCTION_H_
+#define _XH_FUNCTION_H_
 
 #include <tuple>
+#include <type_traits>
 
 namespace xh {
 
-template<typename T>
-struct _remove_mem_fun_const_helper { using type = T; };
-template<typename T>
-struct _remove_mem_fun_volatile_helper { using type = T; };
-template<typename T>
-struct _remove_mem_fun_cv_helper { using type = T; };
+using std::bool_constant, std::false_type, std::true_type, std::remove_cvref_t,
+	std::remove_pointer_t, std::void_t, std::is_member_function_pointer_v,
+	std::is_function_v, std::tuple, std::tuple_element_t;
 
-#define REMOVE_MEM_FUN_CV_HELPER(cv, c_res, v_res, cv_res)                    \
-template<typename Ret, typename Class, typename... Args>                      \
-struct _remove_mem_fun_##cv##_helper<Ret(Class::*)(Args...)> {                \
-  using type = Ret(Class::*)(Args...);                                        \
-};                                                                            \
-template<typename Ret, typename Class, typename... Args>                      \
-struct _remove_mem_fun_##cv##_helper<Ret(Class::*)(Args...) const> {          \
-  using type = Ret(Class::*)(Args...) c_res;                                  \
-};                                                                            \
-template<typename Ret, typename Class, typename... Args>                      \
-struct _remove_mem_fun_##cv##_helper<Ret(Class::*)(Args...) volatile> {       \
-  using type = Ret(Class::*)(Args...) v_res;                                  \
-};                                                                            \
-template<typename Ret, typename Class, typename... Args>                      \
-struct _remove_mem_fun_##cv##_helper<Ret(Class::*)(Args...) const volatile> { \
-  using type = Ret(Class::*)(Args...) cv_res;                                 \
-};
+template<typename _T>
+inline constexpr bool
+	is_function_pointer_v = is_function_v<remove_pointer_t<_T>>;
+template<typename _T>
+struct is_function_pointer : bool_constant<is_function_pointer_v<_T>> {};
 
-REMOVE_MEM_FUN_CV_HELPER(const, , , volatile)
-REMOVE_MEM_FUN_CV_HELPER(volatile, const, , const)
-REMOVE_MEM_FUN_CV_HELPER(cv, , ,)
-#undef REMOVE_MEM_FUN_CV_HELPER
+template<typename, typename = void>
+struct is_functor : false_type {};
+template<typename _T>
+struct is_functor<_T, void_t<decltype(&_T::operator())>> : true_type {};
+template<typename _T>
+inline constexpr bool is_functor_v = is_functor<_T>::value;
 
-template<typename T>
-struct remove_mem_fun_const : _remove_mem_fun_const_helper<T> {};
-template<typename T>
-struct remove_mem_fun_volatile : _remove_mem_fun_volatile_helper<T> {};
-template<typename T>
-struct remove_mem_fun_cv : _remove_mem_fun_cv_helper<T> {};
-template<typename T>
-using remove_mem_fun_const_t = remove_mem_fun_const<T>::type;
-template<typename T>
-using remove_mem_fun_volatile_t = remove_mem_fun_volatile<T>::type;
-template<typename T>
-using remove_mem_fun_cv_t = remove_mem_fun_cv<T>::type;
+template<typename _T,
+	bool _Is = is_function_v<remove_pointer_t<_T>> || is_functor_v<_T>>
+struct _is_callable_helper : bool_constant<_Is> {};
+template<typename _T>
+struct is_callable : _is_callable_helper<remove_cvref_t<_T>> {};
+template<typename _T>
+inline constexpr bool is_callable_v = is_callable<_T>::value;
 
-template<typename Functor>
-struct _function_traits_helper : _function_traits_helper<
-    remove_mem_fun_cv_t<decltype(&Functor::operator())>> {
-};
-template<typename Ret, typename... Args>
-struct _function_traits_helper<Ret(Args...)> {
-  using type = Ret(Args...);
-  using return_type = Ret;
-  using argument_types = std::tuple<Args...>;
-  template<unsigned index>
-  using argument_type = std::tuple_element_t<index, std::tuple<Args...>>;
-  inline static constexpr unsigned arity = sizeof...(Args);
-};
-template<typename Ret, typename... Args>
-struct _function_traits_helper<Ret(*)(Args...)>
-    : _function_traits_helper<Ret(Args...)> {
-};
+template<typename _T> requires is_member_function_pointer_v<_T>
+struct remove_mem_fun_const {};
+template<typename _T> requires is_member_function_pointer_v<_T>
+struct remove_mem_fun_volatile {};
+template<typename _T> requires is_member_function_pointer_v<_T>
+struct remove_mem_fun_cv {};
 
-template<typename Ret, typename... Args>
-struct _function_traits_helper<Ret(Args..., ...)> {
-  using type = Ret(Args..., ...);
-  using return_type = Ret;
-  using argument_types = std::tuple<Args...>;
-  template<unsigned index>
-  using argument_type = std::tuple_element_t<index, std::tuple<Args...>>;
-  inline static constexpr unsigned arity = sizeof...(Args);
+#define REMOVE_MEM_FUN_QUALIFIERS(remove, qualifiers, result)               \
+template<typename _Ret, typename _Class, typename... _Args>                 \
+struct remove_mem_fun_##remove<_Ret(_Class::*)(_Args...) qualifiers> {      \
+  using type = _Ret(_Class::*)(_Args...) result;                            \
+};                                                                          \
+template<typename _Ret, typename _Class, typename... _Args>                 \
+struct remove_mem_fun_##remove<_Ret(_Class::*)(_Args..., ...) qualifiers> { \
+  using type = _Ret(_Class::*)(_Args...) result;                            \
 };
+REMOVE_MEM_FUN_QUALIFIERS(const, ,)
+REMOVE_MEM_FUN_QUALIFIERS(const, const,)
+REMOVE_MEM_FUN_QUALIFIERS(const, volatile, volatile)
+REMOVE_MEM_FUN_QUALIFIERS(const, const volatile, volatile)
+REMOVE_MEM_FUN_QUALIFIERS(volatile, ,)
+REMOVE_MEM_FUN_QUALIFIERS(volatile, const, const)
+REMOVE_MEM_FUN_QUALIFIERS(volatile, volatile,)
+REMOVE_MEM_FUN_QUALIFIERS(volatile, const volatile, const)
+REMOVE_MEM_FUN_QUALIFIERS(cv, ,)
+REMOVE_MEM_FUN_QUALIFIERS(cv, const,)
+REMOVE_MEM_FUN_QUALIFIERS(cv, volatile,)
+REMOVE_MEM_FUN_QUALIFIERS(cv, const volatile,)
+#undef REMOVE_MEM_FUN_QUALIFIERS
 
-template<typename Ret, typename Class, typename... Args>
-struct _function_traits_helper<Ret(Class::*)(Args...)>
-    : _function_traits_helper<Ret(Args...)> {
-};
+template<typename _T>
+using remove_mem_fun_const_t = remove_mem_fun_const<_T>::type;
+template<typename _T>
+using remove_mem_fun_volatile_t = remove_mem_fun_volatile<_T>::type;
+template<typename _T>
+using remove_mem_fun_cv_t = remove_mem_fun_cv<_T>::type;
 
-template<typename T>
-struct function_traits : _function_traits_helper<
-    remove_mem_fun_cv_t < std::remove_cvref_t<T>>> {
+template<typename _T>
+struct _remove_mem_fun_class_helper {};
+template<typename _T, typename _C>
+struct _remove_mem_fun_class_helper<_T(_C::*)> { using type = _T; };
+template<typename _T> requires is_member_function_pointer_v<_T>
+struct remove_mem_fun_class : _remove_mem_fun_class_helper<
+	remove_mem_fun_cv_t<_T>> {
 };
-template<typename T>
-using function_traits_t = function_traits<T>::type;
-template<typename T>
-using function_return_t = function_traits<T>::return_type;
+template<typename _T>
+using remove_mem_fun_class_t = remove_mem_fun_class<_T>::type;
+
+template<typename _T>
+struct _callable_traits_base {};
+template<typename _Ret, typename... _Args>
+struct _callable_traits_base<_Ret(_Args...)> {
+  using type = _Ret(_Args...);
+  using return_type = _Ret;
+  inline static constexpr unsigned arity = sizeof...(_Args);
+  using argument_tuple = tuple<_Args...>;
+  template<unsigned _Idx>
+  using argument_type = tuple_element_t<_Idx, argument_tuple>;
+};
+template<typename _Ret, typename... _Args>
+struct _callable_traits_base<_Ret(_Args..., ...)> {
+  using type = _Ret(_Args..., ...);
+  using return_type = _Ret;
+};
+template<typename _T, typename = void>
+struct _callable_traits_helper : _callable_traits_base<_T> {};
+template<typename _T>
+struct _callable_traits_helper<_T, void_t<decltype(&_T::operator())>>
+	: _callable_traits_base<remove_mem_fun_class_t<
+		decltype(&_T::operator())>> {
+};
+template<typename _T> requires is_callable_v<_T>
+struct callable_traits : _callable_traits_helper<
+	remove_pointer_t<remove_cvref_t<_T>>> {
+};
+template<typename _T>
+using callable_traits_t = callable_traits<_T>::type;
+template<typename _T>
+using callable_return_t = callable_traits<_T>::return_type;
+template<typename _T>
+inline constexpr unsigned callable_arity_v = callable_traits<_T>::arity;
+template<typename _T, unsigned _Idx>
+using callable_argument_t = callable_traits<_T>::template argument_type<_Idx>;
+template<typename _T>
+using callable_argument_tuple = callable_traits<_T>::argument_tuple;
 
 template<typename>
 class mem_fun;
 
-#define MEM_FUN(cv)                                              \
-template<typename Ret, typename Class, typename... Args>         \
-class mem_fun<Ret(Class::*)(Args...) cv> {                       \
-  Ret (Class::*_mem_fun)(Args...) cv;                            \
- public:                                                         \
-  mem_fun(Ret (Class::*f)(Args...) cv) noexcept : _mem_fun(f) {} \
-  Ret operator()(cv Class &obj, Args... args) const {            \
-    return (obj.*_mem_fun)(args...);                             \
-  }                                                              \
-  Ret operator()(cv Class *obj, Args... args) const {            \
-    return (obj->*_mem_fun)(args...);                            \
-  }                                                              \
-};
+#define MEM_FUN(cv)                                                \
+  template <typename Ret, typename Class, typename... Args>        \
+  class mem_fun<Ret (Class::*)(Args...) cv> {                      \
+    Ret (Class::*_mem_fun)(Args...) cv;                            \
+                                                                   \
+   public:                                                         \
+    mem_fun(Ret (Class::*f)(Args...) cv) noexcept : _mem_fun(f) {} \
+    Ret operator()(cv Class &obj, Args... args) const {            \
+      return (obj.*_mem_fun)(args...);                             \
+    }                                                              \
+    Ret operator()(cv Class *obj, Args... args) const {            \
+      return (obj->*_mem_fun)(args...);                            \
+    }                                                              \
+  };
 
 MEM_FUN()
 MEM_FUN(const)
@@ -116,6 +144,6 @@ MEM_FUN(const volatile)
 template<typename T>
 mem_fun(T) -> mem_fun<std::remove_cvref_t<T>>;
 
-}; // namespace xh
+};  // namespace xh
 
-#endif //FUNCTION_TRAITS_H
+#endif  //_XH_FUNCTION_H_
--- main.cpp
+++ main.cpp
@@ -1,18 +1,91 @@
-#include <iostream>
 #include <cstdarg>
-#include "promise.h"
+#include <iostream>
 #include "enum_name.h"
 #include "function.h"
+#include "promise.h"
 
 using std::cout;
 using std::endl;
 using std::is_same_v;
 using namespace xh;
 
-int sum(int a...) { return 0; }
+int fun_0(int) { return 0; }
+auto &fun_1 = fun_0;
+auto &&fun_2 = std::move(fun_0);
+const auto fun_3 = fun_0;
+const auto &fun_4 = fun_0;
+const auto &&fun_5 = std::move(fun_0);
+volatile auto fun_6 = fun_0;
+volatile auto &fun_7 = fun_0;
+volatile auto &&fun_8 = std::move(fun_0);
+const volatile auto fun_9 = fun_0;
+const volatile auto &fun_10 = fun_0;
+const volatile auto &&fun_11 = std::move(fun_0);
+auto *fun_12 = &fun_0;
+auto &fun_13 = fun_12;
+auto &&fun_14 = std::move(fun_12);
+const auto fun_15 = fun_12;
+const auto &fun_16 = fun_12;
+const auto &&fun_17 = std::move(fun_12);
+volatile auto fun_18 = fun_12;
+volatile auto &fun_19 = fun_12;
+volatile auto &&fun_20 = std::move(fun_12);
+const volatile auto fun_21 = fun_12;
+const volatile auto &fun_22 = fun_12;
+const volatile auto &&fun_23 = std::move(fun_12);
+auto fun_24 = [](int...) { return 0; };
+auto &fun_25 = fun_24;
+auto &&fun_26 = std::move(fun_24);
+const auto fun_27 = fun_24;
+const auto &fun_28 = fun_24;
+const auto &&fun_29 = std::move(fun_24);
+volatile auto fun_30 = fun_24;
+volatile auto &fun_31 = fun_24;
+volatile auto &&fun_32 = std::move(fun_24);
+const volatile auto fun_33 = fun_24;
+const volatile auto &fun_34 = fun_24;
+const volatile auto &&fun_35 = std::move(fun_24);
+
+struct A {
+  int fun(int) { return 0; }
+};
 
 int main() {
-  cout << std::is_function_v<decltype([](int a){return a;})> << endl;
-
+  cout << is_same_v<callable_traits_t<decltype(fun_0)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_1)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_2)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_3)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_4)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_5)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_6)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_7)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_8)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_9)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_10)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_11)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_12)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_13)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_14)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_15)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_16)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_17)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_18)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_19)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_20)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_21)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_22)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_23)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_24)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_25)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_26)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_27)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_28)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_29)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_30)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_31)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_32)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_33)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_34)>, int(int)> << endl;
+  cout << is_same_v<callable_traits_t<decltype(fun_35)>, int(int)> << endl;
   return 0;
 }
