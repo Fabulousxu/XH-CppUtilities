820fc285072dfebeacaf34a4a5b954494c19df97
Fabulousxu <1292248830@qq.com>
2024-08-19 20:41:53 +0800

    更新了tutorial

--- cpp_tools.h
+++ cpp_tools.h
@@ -1,123 +1,117 @@
+// C++20 function.h
+// Author: xupeigong@sjtu.edu.cn, 1583913466@qq.com
+// Last Updated: 2024-08-19
+
+// This header file defines several utility components and macros, expanding on
+// certain standard functionalities. It includes features for obtaining the
+// names of enum values at compile-time, implementing an auto_return mechanism,
+// generating default constructors and assignment operators, and providing a
+// comparable interface for creating objects that can be compared using
+// standard comparison operators.
+
 #ifndef _XH_CPP_TOOLS_H_
 #define _XH_CPP_TOOLS_H_
 
-#include <iostream>
+#include <string_view>
 #include "function_traits.h"
 
 using namespace std;
 
 namespace xh {
 
-// enum name
+/*************
+ * enum name *
+ *************/
 
 template<auto>
 constexpr auto enum_name() {
 #if __GNUC__ || __clang__
-	string_view name = __PRETTY_FUNCTION__;
-	size_t start = name.find('=') + 2, end = name.size() - 1;
+  string_view name = __PRETTY_FUNCTION__;
+  size_t start = name.find('=') + 2, end = name.size() - 1;
 #elif _MSC_VER
-	string_view name = __FUNCSIG__;
-	size_t start = name.find('<') + 1, end = name.rfind('>');
+  string_view name = __FUNCSIG__;
+  size_t start = name.find('<') + 1, end = name.rfind('>');
 #endif
-	name = string_view{name.data() + start, end - start};
-	if ((start = name.rfind("::")) != string_view::npos)
-		name = string_view{name.data() + start + 2, name.size() - start - 2};
-	return name.find(')') == string_view::npos ? name : "";
+  name = string_view{name.data() + start, end - start};
+  if ((start = name.rfind("::")) != string_view::npos)
+    name = string_view{name.data() + start + 2, name.size() - start - 2};
+  return name.find(')') == string_view::npos ? name : "";
 }
 
 template<typename T, size_t N = 0>
 constexpr auto enum_max() {
-	if constexpr (!enum_name<static_cast<T>(N)>().empty())
-		return enum_max<T, N + 1>();
-	return N;
+  if constexpr (!enum_name<static_cast<T>(N)>().empty())
+    return enum_max<T, N + 1>();
+  return N;
 }
 
-template<typename T>
-requires is_enum_v<T>
+template<typename T> requires is_enum_v<T>
 constexpr auto enum_name(T value) {
-	constexpr auto num = enum_max<T>();
-	constexpr auto names = []<size_t... N>(std::index_sequence<N...>) {
-		return std::array<std::string_view, num>{
-			enum_name<static_cast<T>(N)>()...};
-	}(std::make_index_sequence<num>{});
-	return names[static_cast<size_t>(value)];
+  constexpr auto num = enum_max<T>();
+  constexpr auto names = []<size_t... N>(std::index_sequence<N...>) {
+    return std::array<std::string_view, num>{enum_name<static_cast<T>(N)>()...};
+  }(std::make_index_sequence<num>{});
+  return names[static_cast<size_t>(value)];
 }
 
 
-// cpp tools
+/***************
+ * auto return *
+ ***************/
 
-#define USE_STD_IO using std::cin, std::cout, std::cerr, std::endl;
-
-/**
- * @brief 接口宏，继承接口时须把子类名称填入模板参数中\
- * @brief 建议只是使用这套标准，而不是直接用这个宏。这样就可以声明带模板的接口了
- * @param InterfaceName 接口类名称
- * @attention 接口中使用 @c Derived 作为继承该接口的类的代词；接口宏是由模板实现的，故不接受模板
-*/
-#define H_INTERFACE(InterfaceName) template<class Derived> class InterfaceName
-
-template<class ...BaseTs>
-class DerivedBaseTs : public BaseTs ... {};
-/**
- * @brief 单例/匿名模式的接口实现辅助宏
- * @param InterfaceNames 要实现的接口基类
- * @attention 只能用来声明变量，不能直接当作变量
- * @example 用法:
- * H_SINGLE_IMPL(TClickEvent){
- *     void onclick() const override{...}
- *     ...
- * } obj;
-*/
-#define H_SINGLE_IMPL(...) class final:public hstd::DerivedBaseTs<__VA_ARGS__>
-
-//可输入类标签
-#define H_INPUTABLE(ClassName) \
-friend std::istream & operator>>(std::istream &ism, ClassName &obj);
-//可输出类标签
-#define H_OUTPUTABLE(ClassName) \
-friend std::ostream & operator<<(std::ostream &osm, const ClassName &obj);
-//可输入输出类标签
-#define H_IOABLE(ClassName) H_INPUTABLE(ClassName) H_OUTPUTABLE(ClassName)
-
-//有指针的类标签
-#define H_HAS_POINTER_MEMBER(ClassName) \
-ClassName(const ClassName &obj);\
-ClassName(ClassName &&obj);\
-~ClassName();\
-ClassName &operator=(const ClassName &obj);\
-ClassName &operator=(ClassName &&obj);
-
-//可比较类(只需要实现compare函数，<0是小于，=0是等于，>0是大于)
-#define H_COMPARABLE(ClassName)\
-bool operator<(const ClassName &obj) const {return compare(obj)<0;}\
-bool operator<=(const ClassName &obj) const {return compare(obj)<=0;}\
-bool operator==(const ClassName &obj) const {return !compare(obj);}\
-bool operator!=(const ClassName &obj) const {return compare(obj);}\
-bool operator>=(const ClassName &obj) const {return compare(obj)>=0;}\
-bool operator>(const ClassName &obj) const {return compare(obj)>0;}\
-int compare(const ClassName &obj) const;
-
-//可比较类接口
-H_INTERFACE(TComparable)
-{
- public:
-	/**
-	 * @brief 比较函数(需要继承实现)
-	 * @param obj 另一个对象
-	 * @return >0为大于, =0为等于, <0为小于
-	*/
-	virtual int compare(const Derived &obj) const = 0;
-	virtual bool operator<(const Derived &obj) const { return compare(obj) < 0; }
-	virtual bool operator<=(const Derived &obj) const {
-		return compare(obj) <= 0;
-	}
-	virtual bool operator==(const Derived &obj) const { return !compare(obj); }
-	virtual bool operator!=(const Derived &obj) const { return compare(obj); }
-	virtual bool operator>=(const Derived &obj) const {
-		return compare(obj) >= 0;
-	}
-	virtual bool operator>(const Derived &obj) const { return compare(obj) > 0; }
+struct auto_return_t {
+  template<class T>
+  operator T() const { return T{}; }
 };
 
-}
+#define auto_return return xh::auto_return_t{};
+
+
+/**********************************************************************
+ * generate default constructors, destructor and assignment operators *
+ **********************************************************************/
+
+#define NO_ARGUMENT_CONSTRUCTOR(class_name) \
+  class_name() = default;
+
+#define DESTRUCTOR(class_name) \
+  ~class_name() = default;
+
+#define COPY_CONSTRUCTOR(class_name) \
+  class_name(const class_name &) = default;
+
+#define MOVE_CONSTRUCTOR(class_name) \
+  class_name(class_name &&) = default;
+
+#define COPY_ASSIGNMENT_OPERATOR(class_name) \
+  class_name &operator=(const class_name &) = default;
+
+#define MOVE_ASSIGNMENT_OPERATOR(class_name) \
+  class_name &operator=(class_name &&) = default;
+
+
+/************************
+ * comparable interface *
+ ************************/
+
+template<class Derived>
+class comparable {
+ public:
+  virtual int compare(const Derived &obj) const = 0;
+  virtual bool operator<(const Derived &obj) const
+  { return compare(obj) < 0; }
+  virtual bool operator<=(const Derived &obj) const
+  { return compare(obj) <= 0; }
+  virtual bool operator==(const Derived &obj) const
+  { return !compare(obj); }
+  virtual bool operator!=(const Derived &obj) const
+  { return compare(obj); }
+  virtual bool operator>=(const Derived &obj) const
+  { return compare(obj) >= 0; }
+  virtual bool operator>(const Derived &obj) const
+  { return compare(obj) > 0; }
+};
+
+};
+
 #endif //_XH_CPP_TOOLS_H_
--- tutorial.cpp
+++ tutorial.cpp
@@ -1,56 +1,107 @@
-#include <iostream>
-#include <cassert>
+// C++20 function.h
+// Author: xupeigong@sjtu.edu.cn, 1583913466@qq.com
+// Last Updated: 2024-08-19
 
+// This file is a tutorial for the XH-CppTools.
+
+#ifndef _XH_TUTORIAL_CPP_
+#define _XH_TUTORIAL_CPP_
+
+#include <cassert>
 #include "function_traits.h"
 #include "function.h"
 #include "cpp_tools.h"
 
-using namespace std;
+// inherit from comparable to implement the compare function
+class A : public xh::comparable<A> {
+ public:
+  int num;
 
-#if 1
+  static const int k = 2;
 
-struct A {
-	int num;
-	static const int k = 2;
+  A(int n) : num(n) {}
 
-	// initialize getter and setter in your class
-	const xh::getter<int> getNumMoreThanK = [&]() { return num + k; };
-	const xh::setter<int> setNumMoreThanK = [&](int n) { num = n - k; };
-	const xh::getset<int, int> numMoreThanK = {
-		[&]() { return num + k; },
-		[&](int n) { num = n - k; }
-	};
+  // generate default constructors and destructor for your class
+  NO_ARGUMENT_CONSTRUCTOR(A)
+  COPY_CONSTRUCTOR(A)
+  MOVE_CONSTRUCTOR(A)
+  DESTRUCTOR(A)
+
+  // initialize getter and setter in your class
+  const xh::getter<int> getNumMoreThanK = [&]() { return num + k; };
+  const xh::setter<int> setNumMoreThanK = [&](int n) { num = n - k; };
+  const xh::getset<int, int> numMoreThanK = {
+      [&]() { return num + k; },
+      [&](int n) { num = n - k; }
+  };
+
+  // implement the compare function
+  int compare(const A &obj) const override { return num - obj.num; }
+
+  int add(int n) volatile {
+    num += n;
+    return num;
+  }
 };
 
+int reload_fun(int) { return 0; }
+char reload_fun(char) { return 'a'; }
+
 int main() {
-	A a{.num = 10};
-	// use getter and setter to get or set some value after processed
-	assert(a.getNumMoreThanK == 12);
-	a.setNumMoreThanK = 13;
-	assert(a.num == 11);
-	assert(a.numMoreThanK == 13);
-	a.numMoreThanK = 14;
-	assert(a.num == 12);
+  A a = 10;
 
-	// use MEMBER_FUNCTION_PROXY to create a proxy for a member function
-	int (A::*ptr)(int);
-	MEMBER_FUNCTION_PROXY(ptr, (int n) -> int, {
-		proxy->num += n;
-		return proxy->num;
-	});
-	assert((a.*ptr)(2) == 14);
+  // use getter and setter to get or set some value after processed
+  assert(a.getNumMoreThanK == 12);
+  a.setNumMoreThanK = 13;
+  assert(a.num == 11);
+  assert(a.numMoreThanK == 13);
+  a.numMoreThanK = 14;
+  assert(a.num == 12);
 
-	// use multi_function to create a function like the reload function
-	xh::multi_function multi_fun = {
-		[](int) { return 0; },
-		[](char) { return 'a'; }
-	};
-	assert(multi_fun(1) == 0);
-	assert(multi_fun('a') == 'a');
-	assert(multi_fun((short)'a') == 0);
+  // use member_function to wrap a member function
+  xh::member_function mf = &A::add;
+  assert(mf(a, 2) == 14);
+  mf(&a, 2);
+  assert(a.num == 16);
 
-	// use auto_return to return a default value
-	auto_return;
+  // use MEMBER_FUNCTION_PROXY to create a proxy for a member function
+  int (A::*ptr)(int);
+  MEMBER_FUNCTION_PROXY(ptr, (int n), {
+    proxy->num += n;
+    return proxy->num;
+  });
+  a.num = 10;
+  assert((a.*ptr)(2) == 12);
+
+  // use multi_function to create a reload-like function
+  xh::multi_function multi_fun = {
+      [](int) { return 0; },
+      [](char) { return 'a'; }
+  };
+  assert(multi_fun(1) == reload_fun(1));
+  assert(multi_fun('a') == reload_fun('a'));
+  assert(multi_fun((short)'a') == reload_fun((short)'a'));
+
+  // use function_chain to call a chain of functions
+  xh::function_chain([](int a, int b) { return a + b; })
+      .then([](int a) { return a * 2; })
+      .then([](int res) { assert(res == (1 + 2) * 2); })
+          (1, 2);
+
+  // function traits
+  auto fun_0 = [](int) { return 0; };
+  const auto &&fun_1 = [](int) { return 0; };
+  static_assert(std::is_same_v<xh::function_traits_t<decltype(fun_0)>,
+                               int(int)>);
+  static_assert(std::is_same_v<xh::function_traits_t<decltype(fun_1)>,
+                               int(int)>);
+  static_assert(std::is_same_v<xh::function_traits_t<decltype(&A::add)>,
+                               int (A::*)(int) volatile>);
+  static_assert(std::is_same_v<xh::function_class_t<decltype(&A::add)>,
+                               A volatile>);
+
+  // use auto_return to return a default value
+  auto_return;
 }
 
-#endif
+#endif //_XH_TUTORIAL_CPP_
